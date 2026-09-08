002569b30654218e56b1b4af85de68170f7d02e9
richmondexpresscouriers <richmondexpresscouriers@outlook.com>
2026-09-08 19:36:52 +0100

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,14 @@
+export const metadata = {
+title: 'Richmond Express Couriers | Delivery Tracking',
+description: 'Track your Richmond Express Couriers delivery',
+}
+
+export default function RootLayout({ children }) {
+return (
+<html lang="en">
+<body style={{ margin: 0 }}>
+{children}
+</body>
+</html>
+)
+}
